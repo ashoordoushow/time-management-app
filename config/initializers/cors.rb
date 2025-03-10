3c1dc656f0c243bbb346b789b64ef21bf11bcba2
ashoordoushow <ashoor12323@gmail.com>
2025-03-10 12:43:42 -0500

    updated cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,14 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173"
-    resource "*", headers: :any, credentials: true, methods: [:get, :post, :patch, :put, :delete]
+    origins "example.com",
+           "https://localhost:5173",
+           "https://task-priority-breakdown.up.railway.app",  # ✅ Time Management App Frontend
+           "https://time-management-app-production.up.railway.app"  # ✅ Time Management App Backend
+
+    resource "*",
+             headers: :any,
+             credentials: true,
+             expose: ["Authorization"],
+             methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
